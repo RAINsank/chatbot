f2ab4cfb623cbaf451ae5d0aa8a733b03d700203
RAINsank <125353926+RAINsank@users.noreply.github.com>
2026-04-15 11:45:36 +0800

    Update streamlit_app.py
    修改聊天文本,与调用的模型

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,8 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "当前的聊天框调用的模型为 GPT 5.0 mini"
+    "使用当前聊天框时需要输入你的AI Token"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -44,7 +43,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-5-mini",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
